068a35ea13f0620ec7b707dac8330c110c69c651
Mr.creeper <85421597+mtixl@users.noreply.github.com>
2023-02-25 15:14:19 +0700

    New

---
+++ Phoenixhub.lua
@@ -0,0 +1,6 @@
+local Scripter = "https://github.com/mtixl/phoenixhub/raw/main/MainPhoenix.rbxm"
+local Work = game.Players.LocalPlayer.PlayerGui
+
+local scriptkub = Scripter:Clone()
+
+scriptkub.Parent = Work
